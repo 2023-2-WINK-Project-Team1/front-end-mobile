ad701cf01f251239d9a552ca6bd4cc2657ee3748
VarGun <heegun4690@naver.com>
2023-11-16 15:56:29 +0900

    eslint, prettier 설정

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>1팀 화이팅 !</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
