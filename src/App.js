ccdbe75a6645c6103b23c35495e2c285aca4ea1b
hyeonjin6530 <cola1876@naver.com>
2023-11-27 16:20:08 +0900

    Implement Button Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from 'styled-components'; // 1. Global color를 사용하기 위한 import
+import theme from './Theme.js'; // 2. Global color를 사용하기 위한 import
+import Button from './component/Button.js'; // Button 컴포넌트 테스트를 위해 잠시 import 해두었습니다.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>1팀 화이팅 !~!!</p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // 3. styled-component의 ThemeProvider와 theme를 가져온 후, 컴포넌트의 최상단에 감싸준다.
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>1팀 화이팅 !~!!</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
