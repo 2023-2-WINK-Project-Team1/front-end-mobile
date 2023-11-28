83763b7db103ab4217128e5a6bcac2e1fa45dc17
hyeonjin6530 <cola1876@naver.com>
2023-11-28 15:39:30 +0900

    Fix Button.js Changes to be committed: modified:   src/component/Button.js 1. props 이름 변경:  children -> btnText 2. cancel과 disabled의 스타일링 위치 변경

--- src/component/Button.js
+++ src/component/Button.js
@@ -43,6 +43,15 @@ const StyledButton = styled.button`
 
   ${sizeStyle}; // 여기서 sizeStyle을 사용 (sizeStyle이 위에서 정의된 후에 사용할 수 있음 -> 아래에서 정의하면 안된다는 뜻)
 
+  /* cancel 버튼도 disabled 처리를 할 일이 있으므로 disabled의 적용 우선 순위를 높여주기 위해 cancel -> disabled 순으로 코드 순서를 변경 */
+  ${(
+    props, // cancel = {true}일 때 변경되는 것들 (Medium 버튼에만 사용되어서 위로 이동해야할 지 고민중)
+  ) =>
+    props.cancel &&
+    css`
+      background-color: ${(props) => props.theme.red};
+    `}
+
   ${(
     props, // disabled = {true}일 때 변경되는 것들 (Large 버튼에만 사용되어서 위로 이동해야할 지 고민중)
   ) =>
@@ -51,28 +60,20 @@ const StyledButton = styled.button`
       background-color: ${(props) => props.theme.gray};
       cursor: not-allowed; // 클릭할 수 없다는 모양의 커서로 변경
     `}
-
-  ${(
-    props, // cancel = {true}일 때 변경되는 것들 (Medium 버튼에만 사용되어서 위로 이동해야할 지 고민중)
-  ) =>
-    props.cancel &&
-    css`
-      background-color: ${(props) => props.theme.red};
-    `}
 `;
 
 function Button(props) {
   /* 사용할 props 정리
-    children: 버튼 안에 들어갈 text를 의미
+    btnText: 버튼 안에 들어갈 text를 의미
     size: 버튼의 크기 값을 의미(Large, Medium, Small)
     disabled: 대여신청, 대여완료, 반납완료 버튼에서의 속성값
     cancel: 대여취소 버튼에서의 속성값
     */
-  const { children, size, disabled, cancel } = props;
+  const { btnText, size, disabled, cancel } = props;
 
   return (
     <StyledButton disabled={disabled} size={size} cancel={cancel}>
-      {children}
+      {btnText}
     </StyledButton>
   );
 }
