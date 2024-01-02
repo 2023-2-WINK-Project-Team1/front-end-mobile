d4574517d5d558c85382747178ab173aeb291a8c
chanwoo <chanwoo0607@naver.com>
2024-01-02 20:52:46 +0900

    front-end-11 Input Component last

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Hello from './passwordInput';
-import Password from "./passwordInput";
-import Input2 from "./Input2";
-import PasswordInput from "./passwordInput";
-import StudentId from "./StudentId";
-import Email from "./Email";
-import Name from "./Name";
-import Count from "./Count";
-import Time from "./Time";
+import PasswordInput from "../input/PasswordInput";
+import StudentId from "../input/StudentId";
+import Email from "../input/Email";
+import Name from "../input/Name";
+import Count from "../input/Count";
+import Time from "../input/Time";
 
 function App() {
     return (
