1380cde1da410fa996e76c81c434d9e375f23878
VarGun <heegun4690@naver.com>
2024-01-03 09:25:00 +0900

    front-end-22 화면 크기 및 비율 설정 + component test 환경 설정

--- src/pages/componentTest/TestPage.js
+++ src/pages/componentTest/TestPage.js
@@ -1,8 +1,24 @@
+import styled from 'styled-components';
+
+const HorizontalWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const VerticalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 function TestPage() {
   return (
-    <div>
-      <h1>test page 입니다</h1>
-    </div>
+    <>
+      <HorizontalWrapper>
+        <h1>가로 정렬 </h1>
+      </HorizontalWrapper>
+      <VerticalWrapper>
+        <h1>세로 정렬 </h1>
+      </VerticalWrapper>
+    </>
   );
 }
 
