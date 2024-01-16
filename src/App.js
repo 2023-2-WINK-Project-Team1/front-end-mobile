48208d98949c675a3f669f2cdd1d854641fbbba2
한승훈 <seunghoon526@naver.com>
2024-01-17 01:27:43 +0900

    front-end-26 Setting Page 개발

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import theme from './Theme.js'; // 2. Global color를 사용하기 위한 import
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
-// import NewAlarm from './pages/NewAlarm';
-import NewSetting from './pages/NewSetting';
+import Setting from './pages/Setting';
 import TestPage from './pages/componentTest/TestPage';
 
 function App() {
@@ -17,9 +16,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Main />}></Route>
             {/*<Route path="/alarm" element={<NewAlarm />}></Route>*/}
-            {/*alarm 에서 newalarm을 불러온다*/}
-            <Route path="/setting" element={<NewSetting />}></Route>
-            <Route path="/testPage" element={<TestPage />}></Route>
+            {/*localhost/alarm에 NewAlarm을 불러오겠다 이거임*/}
+            <Route path="/setting" element={<Setting />}></Route>
+            <Route path="/test-page" element={<TestPage />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
