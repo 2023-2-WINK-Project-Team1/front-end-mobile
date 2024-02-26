fb3063532149f5c1cf72375ad1a51c15bcd452b5
VarGun <heegun4690@naver.com>
2024-02-27 02:19:05 +0900

    Chore: 색상 변수 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { ThemeProvider } from 'styled-components'; // 1. Global color를 사용하기 위한 import
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
@@ -23,7 +22,6 @@ import GoodsManagement from './pages/GoodsManagement';
 
 function App() {
   return (
-    // 3. styled-component의 ThemeProvider와 theme를 가져온 후, 컴포넌트의 최상단에 감싸준다.
     <RecoilRoot>
       <div id="App">
         <CookiesProvider>
